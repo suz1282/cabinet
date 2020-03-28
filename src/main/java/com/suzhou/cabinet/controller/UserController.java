daf7ec8ff68235d6871d702e14fc3364fb18e9d5
SUZ <2412500736@qq.com>
2020-03-28 10:28:30 +0800

    静态前端

--- src/main/java/com/suzhou/cabinet/controller/UserController.java
+++ src/main/java/com/suzhou/cabinet/controller/UserController.java
@@ -7,14 +7,12 @@ import com.suzhou.cabinet.utils.RestResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.sql.Connection;
+import java.sql.Driver;
 
 /**
  * <p>
@@ -25,17 +23,33 @@ import javax.validation.Valid;
  * @since 2020-03-23
  */
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/user")
 @Api(description = "用户管理")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @GetMapping("/getUser")
+    @ApiOperation("获取用户")
+    public User getUser() {
+        User u =new User();
+        u.setId("123");
+        u.setName("suzhou");
+        return u;
+    }
+
     @PostMapping("/login")
     @ApiOperation("用户登录")
-    public RestResult<String> login(@Valid @RequestBody User loginUser) {
-        return userService.login(loginUser);  //支持手机号登录（使用密码）
+    public RestResult<String> login(@RequestBody User loginUser) {
+        return userService.login(loginUser);
     }
+    @PostMapping("/register")
+    @ApiOperation("用户注册")
+    public RestResult<String> register(@RequestBody User loginUser) {
+        return userService.login(loginUser);
+    }
+
 }
 
